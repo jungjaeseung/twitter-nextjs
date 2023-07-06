27ef4d4d9af587a17404cbdee0f3cde02d6d2898
3Harang <jungjaeseung9475@gmail.com>
2023-07-06 16:29:43 +0900

    Login, Auth with modals

--- components/modals/RegisterModal.tsx
+++ components/modals/RegisterModal.tsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { useCallback, useState } from "react";
+import { toast } from "react-hot-toast";
+import { signIn } from "next-auth/react";
 
 import useLoginModal from "@/hooks/useLoginModal";
 import useRegisterModal from "@/hooks/useRegisterModal";
@@ -37,9 +39,17 @@ const RegisterModal = () => {
         name,
       });
 
+      toast.success("Account created.");
+
+      signIn("credentials", {
+        email,
+        password,
+      });
+
       registerModal.onClose();
     } catch (error) {
       console.log(error);
+      toast.error("Something went wrong");
     } finally {
       setIsLoading(false);
     }
@@ -67,6 +77,7 @@ const RegisterModal = () => {
       />
       <Input
         placeholder="Password"
+        type="password"
         onChange={(e) => setPassword(e.target.value)}
         value={password}
         disabled={isLoading}
