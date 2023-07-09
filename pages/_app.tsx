af0648f7f49c49d8b4d34a45215f59c62f143433
3Harang <jungjaeseung9475@gmail.com>
2023-07-09 21:19:11 +0900

    Profile Edit Modal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,16 @@ import { Toaster } from "react-hot-toast";
 import { SessionProvider } from "next-auth/react";
 
 import Layout from "@/components/Layout";
-import "@/styles/globals.css";
 import LoginModal from "@/components/modals/LoginModal";
 import RegisterModal from "@/components/modals/RegisterModal";
+import EditModal from "@/components/modals/EditModal";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Toaster />
+      <EditModal />
       <RegisterModal />
       <LoginModal />
       <Layout>
