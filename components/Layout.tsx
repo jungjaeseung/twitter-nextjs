09afb81e0c0cf02c01856ef53c6fc21f0c5b1201
3Harang <jungjaeseung9475@gmail.com>
2023-07-12 17:36:35 +0900

    Responsive design - mobile layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,11 +9,12 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="h-screen bg-black">
       <div className="container h-full mx-auto xl:px-30 max-w-6xl">
-        <div className="grid grid-cols-4 h-full">
+        <div className="grid grid-cols-5 lg:grid-cols-4 h-full ">
           <Sidebar />
           <div
             className="
-          col-span-3
+          col-span-4
+          md:col-span-3
           lg:col-span-2
           border-x-[1px]
           border-neutral-800
